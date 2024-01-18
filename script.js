75437b152653d0d16c71fe43a7da1ee9736f3d2c
sajinrajan <sajinrajan082@gmail.com>
2024-01-18 08:49:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function toggleFullscreen(imageSrc) {
+    console.log('called');
+
+    console.log(imageSrc);
+    const element = document.documentElement;
+
+    if (!document.fullscreenElement) {
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    }
+
+    // Change the image source
+    const fullscreenImage = document.getElementById('fullscreenImage');
+    fullscreenImage.src = imageSrc;
+  }
